3e5f54e2eecdcd4ddce0bd543a1bac709c7097c8
Rohini Kumari <krirohini@gmail.com>
2019-02-12 15:13:20 -0800

    smallest and largest in given array

---
+++ Common_Programs/com/rohini/array/SmallestAndLargest_In_UnsortedArray.py
@@ -0,0 +1,37 @@
+'''
+Created on Feb 12, 2019
+
+@author: r.kumari
+'''
+import array
+
+class SmallestAndLargest_In_UnsortedArray:
+
+    def smallestAndLargest(self, givenArray):
+        
+        length = len(givenArray)
+        
+        smallest = 0
+        largest = 0
+        
+        i = 0
+        while i < length:
+            print(">>>> i >>>>", i)
+            if givenArray[i] < smallest:
+                smallest = givenArray[i]
+            else:
+                smallest = smallest
+                
+            if givenArray[i] > largest:
+                largest = givenArray[i]
+            else:
+                largest = largest
+            
+            i = i + 1
+            
+        print (">>>> givenArray >>>> ", givenArray)
+        print (">>>> smallest >>>> ", smallest)
+        print (">>>> largest >>>> ", largest)
+        
+obj = SmallestAndLargest_In_UnsortedArray()
+obj.smallestAndLargest([1,4, 2, 9, 0, -5, -2])
